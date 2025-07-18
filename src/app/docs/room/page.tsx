a20949731689629c0956b23ce21bca6b57003a9a
lin <88231954+littlePrincessAlin@users.noreply.github.com>
2025-07-18 11:43:27 +0800

    fix: 修复拖拽手柄触发时机为focus态 (#27)
    * fix: 修复拖拽手柄触发时机为focus态
    
    * feat: ui适配
    
    ---------
    
    Co-authored-by: lilin <lilin@bailongma-inc.com>

--- src/app/docs/room/page.tsx
+++ src/app/docs/room/page.tsx
@@ -37,7 +37,7 @@ export default function Document() {
 
   const {
     editor,
-    isEditable,
+    // isEditable,
     connectionStatus,
     provider,
     isOffline,
@@ -181,7 +181,7 @@ export default function Document() {
 
       {editor && (
         <>
-          <ContentItemMenu editor={editor} isEditable={isEditable} />
+          <ContentItemMenu editor={editor} />
           <LinkMenu editor={editor} appendTo={menuContainerRef} />
           <TextMenu editor={editor} documentId={roomId} />
           <ColumnsMenu editor={editor} appendTo={menuContainerRef} />
