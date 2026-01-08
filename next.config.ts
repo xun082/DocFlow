fabfbe091262da3529f4b6fc33dfacc6a80fca75
Moment <73689580+xun082@users.noreply.github.com>
2026-01-08 18:34:30 +0800

    fix: 🧩 sentry internal server error for monitoring endpoint (#221)

--- next.config.ts
+++ next.config.ts
@@ -232,7 +232,6 @@ export default withSentryConfig(withAnalyzer(nextConfig), {
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
   org: 'docflow-0c',
-
   project: 'javascript-nextjs',
 
   // Only print logs for uploading source maps in CI
@@ -244,18 +243,9 @@ export default withSentryConfig(withAnalyzer(nextConfig), {
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: '/monitoring',
-
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
+  // Enables automatic instrumentation of Vercel Cron Monitors
   automaticVercelMonitors: true,
 });
